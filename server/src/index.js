bc017caddd6ffcb2e62971f4913fbf2c6da9dcb3
Arpit Kashyap <arpitkashyap2000@gmail.com>
2025-06-17 08:05:01 +0000

    cors enable

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 // Configure CORS
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://live-polling-system-ten.vercel.app/"],
   methods: ["GET", "POST"],
   credentials: true
 };
